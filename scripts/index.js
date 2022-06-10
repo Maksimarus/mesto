70610abedb4d707d184d7f929a5d1423a7c8afdd
Maksim Chuchuk <cucukmaksim@gmail.com>
2022-06-10 13:34:03 +0300

    fix: исправить верстку и скрипт попапа

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+const popupCloseButton = document.querySelector('.popup__close-button');
+
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('#nameInput');
+const jobInput = document.querySelector('#jobInput');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+// Открытие попапа при клике на кнопку изменения
+profileEditButton.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
+});
+
+// Закрыти попапа при клике на кнопку закрыть(крестик)
+popupCloseButton.addEventListener('click', () => {
+  closePopup();
+});
+
+// Функция-обработчик отправки формы (вместо отправки - изменение информации в профиле)
+function formSubmitHandler(e) {
+  console.log('111');
+  e.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+
+  closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
